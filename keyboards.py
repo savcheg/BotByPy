e922d15f42280bcb81853226988b7cdee1cf6a46
Spichkenss <72526058+Spichkenss@users.noreply.github.com>
2021-05-22 21:34:29 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -15,30 +15,3 @@ def greetkeyboard():
     start_keyboard.add(keyboard_magictext)
     start_keyboard.add(keyboard_anekdot)
     return start_keyboard
-
-# Клавиатура для калькулятора
-def calckeyboard():
-    calckeyboard = telebot.types.InlineKeyboardMarkup()
-    calckeyboard.row (  telebot.types.InlineKeyboardButton('C', callback_data='C'),
-                    telebot.types.InlineKeyboardButton('⬅', callback_data='<='),
-                    telebot.types.InlineKeyboardButton('➗', callback_data='/'))
-
-    calckeyboard.row (  telebot.types.InlineKeyboardButton('7️⃣', callback_data='7'),
-                    telebot.types.InlineKeyboardButton('8️⃣', callback_data='8'),
-                    telebot.types.InlineKeyboardButton('9️⃣', callback_data='9'),
-                    telebot.types.InlineKeyboardButton('✖', callback_data='*'))
-
-    calckeyboard.row (  telebot.types.InlineKeyboardButton('4️⃣', callback_data='4'),
-                    telebot.types.InlineKeyboardButton('5️⃣', callback_data='5'),
-                    telebot.types.InlineKeyboardButton('6️⃣', callback_data='6'),
-                    telebot.types.InlineKeyboardButton('➖', callback_data='-'))
-
-    calckeyboard.row (  telebot.types.InlineKeyboardButton('1️⃣', callback_data='1'),
-                    telebot.types.InlineKeyboardButton('2️⃣', callback_data='2'),
-                    telebot.types.InlineKeyboardButton('3️⃣', callback_data='3'),
-                    telebot.types.InlineKeyboardButton('➕', callback_data='+'))
-
-    calckeyboard.row (  telebot.types.InlineKeyboardButton('=', callback_data='='),
-                    telebot.types.InlineKeyboardButton('0️⃣', callback_data='0'),
-                    telebot.types.InlineKeyboardButton(',', callback_data=','))
-    return calckeyboard
